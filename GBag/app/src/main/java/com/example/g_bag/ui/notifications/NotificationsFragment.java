ff45b46766bb0cfaf011c706b30dacf44f51fa7a
Eileen <eileen_sanchez@hotmail.es>
2020-07-09 22:44:26 -0500

    se agrega boton de cierre de sesion

--- GBag/app/src/main/java/com/example/g_bag/ui/notifications/NotificationsFragment.java
+++ GBag/app/src/main/java/com/example/g_bag/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,11 @@
 package com.example.g_bag.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +14,31 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.g_bag.Login;
+import com.example.g_bag.Preferences;
 import com.example.g_bag.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    Button btnCerrarSesion;
+    View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        btnCerrarSesion = root.findViewById(R.id.btnCerrarSesion);
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Preferences.LimpiarCredenciales(getContext());
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+                getActivity().finish();
             }
         });
+
         return root;
     }
 }
