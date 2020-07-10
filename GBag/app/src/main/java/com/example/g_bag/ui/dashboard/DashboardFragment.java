732cd3b39a16d128eca47c3fcc997b9efd75e9ac
Gabriel A. Murillo <gabanmur@espol.edu.ec>
2020-07-10 08:22:51 -0500

    Calculo de distancia radial

--- GBag/app/src/main/java/com/example/g_bag/ui/dashboard/DashboardFragment.java
+++ GBag/app/src/main/java/com/example/g_bag/ui/dashboard/DashboardFragment.java
@@ -1,30 +1,111 @@
 package com.example.g_bag.ui.dashboard;
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 
 import com.example.g_bag.R;
+import com.google.android.gms.dynamic.SupportFragmentWrapper;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DashboardFragment extends FragmentActivity implements OnMapReadyCallback {
+import java.text.DecimalFormat;
+
+public class DashboardFragment extends Fragment implements OnMapReadyCallback{
+
+    GoogleMap mMap;
+    public LatLng UBICACIONU = new LatLng(-0.947334, -80.732324);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+        return root;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_dashboard);
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UBICACIONU));
+        Marker mochila;
+        Marker ubicacion;
+        LatLng MOCHILAU = new LatLng(-0.946630, -80.734113);
+        mochila = mMap.addMarker(new MarkerOptions().position(MOCHILAU).title("Mochila " + String.valueOf(CalculationByDistance(MOCHILAU,UBICACIONU)) + " Km").draggable(true).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        ubicacion = mMap.addMarker(new MarkerOptions().position(UBICACIONU).title("Ubicacion"));
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setMarkerDragListener(mMap);
+
     }
 
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+    public double CalculationByDistance(LatLng StartP, LatLng EndP) {
+        int Radius = 6371;// radius of earth in Km
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.valueOf(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.valueOf(newFormat.format(meter));
+        Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                + " Meter   " + meterInDec);
+
+        return Radius * c;
     }
+
+    public void setMarkerDragListener(GoogleMap map) {
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                LatLng p = marker.getPosition();
+                marker.setTitle("Mochila " + String.valueOf(CalculationByDistance(p, UBICACIONU) + " Km"));
+            }
+        });
+    }
+
+
 }
